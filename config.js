194f0ca5ac69fdd395b009565db41ed193c8412b
wyyzxml2330 <104090480+wyyzxml2330@users.noreply.github.com>
2022-04-21 01:00:33 +0800

    Update config.js

--- config.js
+++ config.js
@@ -29,7 +29,7 @@ var config = {
      * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
      */
     imgs: {
-        "揪咪": "file:///Users/chenshijia/Downloads/未命名文件夹/IMG_7556.jpg",
+        "揪咪": "./imgs/IMG_7556.jpg",
         // "今天是你的生日": "./imgs/IMG_7556.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
